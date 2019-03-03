78ceafd1235b7870c8acf4592d011515fc1e543a
Anil Varghese <anil.varghese@care.com>
2019-03-03 12:10:08 +0530

    initial commit

---
+++ Customer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Customer
+//
+//  Created by Anil Varghese on 03/03/19.
+//  Copyright © 2019 Anil Varghese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
